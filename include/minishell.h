1c114e45f4b9829993ffbce16b53e7beef559022
Clement Deville <cdeville@paul-f3Br4s6.clusters.42paris.fr>
2024-04-09 13:32:43 +0200

    Replacing some spaces by tabs

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 15:09:27 by cdeville          #+#    #+#             */
-/*   Updated: 2024/04/09 11:08:10 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/04/09 13:31:07 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,41 +36,41 @@ t_dblist	*generate_env(char **envp);
 
 void		print_env(void *content);
 
-typedef enum  s_token_type
+typedef enum s_token_type
 {
-    TOKEN_ELSE,
-    TOKEN_RED_IN, // --> '<'
-    TOKEN_RED_OUT, // --> '>'
-    TOKEN_PIPE, // --> '|'
-    TOKEN_AND, // --> '&&'
-    TOKEN_OR, // --> '||'
-    TOKEN_VAR_ENV, // --> '$'
-    TOKEN_DELIM_L, // --> '<<'
-    TOKEN_DELIM_G, // --> '>>'
-    TOKEN_SUBSHELL_OPEN, // --> '('
-    TOKEN_SUBSHELL_CLOSE, // --> ')'
+	TOKEN_ELSE,
+	TOKEN_RED_IN, // --> '<'
+	TOKEN_RED_OUT, // --> '>'
+	TOKEN_PIPE, // --> '|'
+	TOKEN_AND, // --> '&&'
+	TOKEN_OR, // --> '||'
+	TOKEN_VAR_ENV, // --> '$'
+	TOKEN_DELIM_L, // --> '<<'
+	TOKEN_DELIM_G, // --> '>>'
+	TOKEN_SUBSHELL_OPEN, // --> '('
+	TOKEN_SUBSHELL_CLOSE, // --> ')'
 
-} t_token_type;
+}	t_token_type;
 
 
-typedef struct  s_token
+typedef struct _token
 {
-    t_token_type    type;
-    char            *value;
-    char            *prev;
-    char            *next;
-}   t_token;
+	t_token_type	type;
+	char			*value;
+	char			*prev;
+	char			*next;
+}	t_token;
 
-typedef struct  s_mini_env
+typedef struct s_mini_env
 {
-    char    *line;
-    char    **env;
-    t_token *tokens;
-}   t_mini_env;
+	char	*line;
+	char	**env;
+	t_token	*tokens;
+}	t_mini_env;
 
 
-void ft_tokenization(t_mini_env *ms);
-void    ft_init_env(char **env, t_mini_env *ms, char *line);
-void    lst_token_add_back(t_token **token_list, t_token *new);
+void		ft_tokenization(t_mini_env *ms);
+void		ft_init_env(char **env, t_mini_env *ms, char *line);
+void		lst_token_add_back(t_token **token_list, t_token *new);
 
 #endif
