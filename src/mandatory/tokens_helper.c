91358a9bf7d37a97cc0aef401aa38c39f9d5f344
skapersk <simon.kaperski@gmail.com>
2024-04-10 00:46:52 +0200

    quotes handling + 1 missing

--- src/mandatory/tokens_helper.c
+++ src/mandatory/tokens_helper.c
@@ -6,7 +6,7 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:46:40 by skapersk          #+#    #+#             */
-/*   Updated: 2024/04/10 00:42:00 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/04/10 00:46:03 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,5 @@ int	there_is_quotes(char *tmp)
 				i++;
 		}
 	}
-	return (i);
+	return (i + 1);
 }
