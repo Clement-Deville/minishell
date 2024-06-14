d7715eaaf6d9644ba271fc42ca64739647c4f77d
Clement Deville <cdeville@paul-f3Br4s6.clusters.42paris.fr>
2024-06-14 11:39:10 +0200

    Patch PWD

--- src/mandatory/pwd.c
+++ src/mandatory/pwd.c
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 11:56:01 by cdeville          #+#    #+#             */
-/*   Updated: 2024/06/11 17:11:23 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/06/14 11:38:02 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,13 @@ int	do_pwd(t_node *node)
 {
 	char	*current_dir_name;
 
-	if (node->c_cmd->expand[1])
-	{
-		ft_putendl_fd("pwd: too many arguments", 2);
-		return (1);
-	}
 	current_dir_name = getcwd(NULL, 0);
 	if (current_dir_name == NULL)
-		return (perror("Pwd error"), 1);
+	{
+		if (node->silent == FALSE)
+			perror("Pwd error");
+		return (1);
+	}
 	ft_putstr_fd(current_dir_name, 1);
 	ft_putchar_fd('\n', 1);
 	free(current_dir_name);
