c4976b0ebdd4eac6d2d3819d0a37e4897a41e505
skapersk <simon.kaperski@gmail.com>
2024-06-19 20:40:29 +0200

    patch heredoc after command

--- src/mandatory/parser_utils_nodes.c
+++ src/mandatory/parser_utils_nodes.c
@@ -6,7 +6,7 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 13:20:21 by skapersk          #+#    #+#             */
-/*   Updated: 2024/06/17 17:23:57 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/06/19 20:38:47 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,7 @@ int	ft_get_red_node(t_red_node **node, t_mini_env *ms)
 		ms->tokens = ms->tokens->next;
 		if (!ms->tokens || ms->tokens->type != TOKEN_ELSE)
 			return (ft_set_parse_err(E_SYNTAX), get_ms()->tmp = tmp, 0);
-		if (red_type == TOKEN_HERE_DOC && ms->tokens->next->type != TOKEN_ELSE)
+		if ( ms->tokens->type != TOKEN_ELSE)
 			return (ft_set_parse_err(E_SYNTAX),
 				get_ms()->tmp = ms->tokens->next, 0);
 		tmp_red = ft_create_red_node(red_type, ms->tokens->value);
